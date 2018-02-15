ff6103b2b646b8fcc07b46ab93ddbc91d3875192
Tianhao <zhangth1990@hotmail.com>
2018-02-14 23:19:16 -0700

    add enemy laser

---
+++ Laser Defender/Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+   
+    
+    public GameObject projectile;
+    public float health = 250;
+    public float projectileSpeed = 10;
+
+
+    void Update()
+    {
+        Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
+        GameObject missile = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+        missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+        {
+            Projectile missile= collider.gameObject.GetComponent<Projectile>();
+            if (missile)
+            {
+                health -= missile.getDamage();
+                missile.Hit();
+                if (health <= 0){
+                    Destroy(gameObject);
+                }
+            }
+        }
+}
+
